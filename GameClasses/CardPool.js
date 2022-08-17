75aa6312aa0d07959089ede4f5c7fd2eebe07c64
Sebastian Rodriguez <>
2022-08-17 16:53:44 +0000

    Started work on restoring old files.

---
+++ GameClasses/CardPool.js
@@ -0,0 +1,39 @@
+class CardPool {
+
+  #sortedPool;
+
+  constructor(cardPool){
+
+    this.#sortedPool = new Map;
+    this.#sortedPool.set("land", []);
+    this.#sortedPool.set("common", []);
+    this.#sortedPool.set("uncommon", []);
+    this.#sortedPool.set("rare", []);
+    this.#sortedPool.set("uncommonMythic", []);
+    
+  }
+
+  getLand(){
+
+    
+  }
+
+  getCommon(){
+
+
+    
+  }
+
+  getUncommon(){
+
+    
+  }
+  getRare(){
+
+    
+  }
+  getMythic(){
+
+    
+  }
+}
---
+++ GameClasses/PlayerRoster.js
@@ -0,0 +1,28 @@
+//keeps track of all active players in all games.
+class PlayerRoster {
+
+  static allPlayers = new Set();
+
+  static addPlayers(players){
+
+    for(let player of players){
+
+      if(this.allPlayers.has(player)){
+
+        return false;
+        
+      }
+      
+    }
+
+    for(let player of players){
+
+      this.allPlayers.add(player);
+      
+    }
+
+    return true;
+    
+  }
+  
+}
